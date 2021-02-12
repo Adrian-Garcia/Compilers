247cd68880db7eba4eb2d1974291bbaa20fca694
Adrian-Garcia <adriangarlop@indexzero.io>
2021-02-12 11:32:12 -0600

    Refractored code

--- Primer Parcial/Tarea1/stack.py
+++ Primer Parcial/Tarea1/stack.py
@@ -7,18 +7,15 @@ class Stack:
 	def push(self, value):
 		node = Node(value, None)
 
-		if self.head == None:
-			self.head = node
-		else:
-			prevHead = self.head
-			self.head = node
-			self.head.next = prevHead
+		prevHead = self.head
+		self.head = node
+		self.head.next = prevHead
 
 		return True
 
 	def pop(self):
 		if self.head == None:
-			return 
+			return
 
 		headVal = self.head.val
 		self.head = self.head.next
